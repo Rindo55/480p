94688580623ee831345a7cfbecd2ce4979144758
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-21 10:06:56 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -134,7 +134,7 @@ async def start_uploading(data):
         filed = filed.replace("[SubsPlease]", "")
         filed = filed.replace("Shinka", "Shin Shinka")
         filed = filed.replace("(480p)", "[480p Web-DL].mkv")
-        KAYO_ID = -1001159872623
+        KAYO_ID = -1001723145599
         ghostname = name
         ghostname = ghostname.replace("(480p)", "")
         
